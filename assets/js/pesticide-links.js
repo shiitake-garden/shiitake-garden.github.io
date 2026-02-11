e6cd866de04e5f9d97ff349f179066d865e810a7
shiitake-garden <fuurainomori@gmail.com>
2026-02-11 10:53:44 +0900

    Update pesticide-links.js

--- assets/js/pesticide-links.js
+++ assets/js/pesticide-links.js
@@ -1,10 +1,9 @@
-<script>
 const pesticideLinks = {
   "スミチオン": "https://amzn.to/3OsRoDS",
   "ダコニール": "https://amzn.to/46EAJn5",
   "モスピラン": "https://amzn.to/4qtxB4L",
-  "ダントツ": "https://amzn.to/4kljwVn",   
-　"プレオ": "https://amzn.to/4r4Oiob",
+  "ダントツ": "https://amzn.to/4kljwVn",
+  "プレオ": "https://amzn.to/4r4Oiob",
   "スターマイト": "https://amzn.to/3ZY2TFS",
   "カネマイト": "https://amzn.to/3OsRKdG",
   "フェニックス": "https://amzn.to/3Mnyhue",
@@ -16,7 +15,7 @@ const pesticideLinks = {
   "マラソン": "https://amzn.to/4aBk1Ye",
   "ディアナ": "https://amzn.to/4a5pQNw",
   "コロマイト": "https://amzn.to/46Ev8x4",
-　"ウララDF": "https://amzn.to/4tvBk4z",
+  "ウララDF": "https://amzn.to/4tvBk4z",
   "スピノエース": "https://amzn.to/3ZY3mb6",
   "ダニサラバ": "https://amzn.to/3ObkmYQ",
   "スタークル": "https://amzn.to/4apepiz",
@@ -24,16 +23,5 @@ const pesticideLinks = {
   "ダニトロン": "https://amzn.to/4qob616",
   "オルトラン粒剤": "https://amzn.to/3Zkduea",
   "ダイアジノン": "https://amzn.to/4algR9E",
-  "トップジンM": "https://amzn.to/46EAKYb" 
+  "トップジンM": "https://amzn.to/46EAKYb"
 };
-</script>
-<script>
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".pesticide").forEach(el => {
-    const name = el.textContent.trim();
-    if (pesticideLinks[name]) {
-      el.innerHTML = `<a href="${pesticideLinks[name]}" target="_blank">${name}</a>`;
-    }
-  });
-});
-</script>
